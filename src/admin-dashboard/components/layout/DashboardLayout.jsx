90d2c1b1b37271603811860697a04b1e82432fda
Ahmed Hamdy <syda90215@gmail.com>
2026-09-06 00:58:54 +0300

    last update

--- src/admin-dashboard/components/layout/DashboardLayout.jsx
+++ src/admin-dashboard/components/layout/DashboardLayout.jsx
@@ -26,7 +26,7 @@ export default function DashboardLayout() {
         isMobileMenuOpen={isMobileMenuOpen} 
         setIsMobileMenuOpen={setIsMobileMenuOpen} 
       />
-      <div className="flex-1 ltr:lg:ml-60 rtl:lg:mr-60 flex flex-col min-h-screen w-full transition-all duration-300">
+      <div className="flex-1 min-w-0 ltr:lg:ml-60 rtl:lg:mr-60 flex flex-col min-h-screen transition-all duration-300">
         <Topbar setIsMobileMenuOpen={setIsMobileMenuOpen} />
         <main className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
           <Outlet />
